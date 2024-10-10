e83db6b08f899776a56507306968751f564ff24f
Jéssica Prestelo <jessica.p.jesus@aluno.senai.br>
2024-10-10 15:45:55 -0300

    Feat: Finalização dos requisitos

--- routes/suspeitos.routes.js
+++ routes/suspeitos.routes.js
@@ -29,3 +29,42 @@ let suspeitos = [
 
   }
 ];
+
+// Rota para listar todos os suspeitos
+suspeitosRoutes.get("/", (req, res) => {
+  return res.status(200).json(suspeito);
+});
+
+// Rota para buscar um suspeito pelo id
+suspeitosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+    // Busca um suspeito pelo id no array de suspeitos
+    const suspeito = suspeitos.find((perseguido) => perseguido.id == id);
+
+    // Verifica se o suspeito foi encontrado
+    if (!suspeito) {
+      return res
+        .status(404)
+        .json({ message: `Suspeito com id ${id} não encontrado!` });
+    }
+  
+    return res.status(200).json(suspeito);
+  });
+
+// Rota para atualizar um suspeito pelo id
+suspeitosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, profissão, nivelSuspeita, Envolvimento} = req.body;
+});
+
+
+  // Remove o suspeito do array de suspeitos
+  suspeitos = suspeitos.filter((suspeito) => suspeito.id != id);
+
+  return res.status(200).json({
+    message: "Suspeito removido com sucesso!",
+    suspeito,
+  });
+
+export default suspeitosRoutes;
